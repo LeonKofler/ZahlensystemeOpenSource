2da40ba042cd5ae272138a93fb5af581ae1ee50c
LeonKofler <62984671+LeonKofler@users.noreply.github.com>
2020-04-28 11:02:02 +0200

    HEX Zahlen Umwandeln

--- ZahlensystemeOpenSource/ZahlensystemeOpenSource/Program.cs
+++ ZahlensystemeOpenSource/ZahlensystemeOpenSource/Program.cs
@@ -11,5 +11,14 @@ namespace ZahlensystemeOpenSource
         {
             Console.ReadKey();
         }
+
+        static string ConvertToHex(string Eingelesen)
+        {
+            string Erg = Convert.ToInt32(Eingelesen, 2).ToString("X");
+
+            return Erg;
+        }
+
+
     }
 }
